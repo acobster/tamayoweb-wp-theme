93ac74f0a86aab31df8c347187f6f11e7513700a
The Ambassador of Funk <acobster@gmail.com>
2014-06-09 15:37:19 -0700

    look and feel

--- index.php
+++ index.php
@@ -1,30 +1,18 @@
 <?php get_header(); ?>
+
 <div class="row">
   <div class="small-12 large-8 columns" role="main">
 
-  <?php if ( have_posts() ) : ?>
+  <?php do_action('foundationPress_before_content'); ?>
 
-    <?php do_action('foundationPress_before_content'); ?>
-
-    <?php while ( have_posts() ) : the_post(); ?>
-      <?php get_template_part( 'content', get_post_format() ); ?>
-    <?php endwhile; ?>
-
-    <?php else : ?>
-      <?php get_template_part( 'content', 'none' ); ?>
-
-    <?php do_action('foundationPress_before_pagination'); ?>
-
-  <?php endif;?>
-
-
-
-  <?php if ( function_exists('FoundationPress_pagination') ) { FoundationPress_pagination(); } else if ( is_paged() ) { ?>
-    <nav id="post-nav">
-      <div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'FoundationPress' ) ); ?></div>
-      <div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'FoundationPress' ) ); ?></div>
-    </nav>
-  <?php } ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+      <?php do_action('foundationPress_page_before_entry_content'); ?>
+      <div class="entry-content">
+        <?php the_content(); ?>
+      </div>
+    </article>
+  <?php endwhile;?>
 
   <?php do_action('foundationPress_after_content'); ?>
 
