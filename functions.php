60a385281438e08c004745363e6479a92fd2204d
The Ambassador of Funk <acobster@gmail.com>
2014-06-09 10:13:21 -0700

    comments

--- functions.php
+++ functions.php
@@ -12,11 +12,22 @@ require_once('library/actions.php');
 
 
 
+/**
+ * Get a URI for a given image file (assumes the file is in the standard theme
+ * image directory)
+ * @param  string the file name
+ * @return string the URI
+ */
 function image_uri( $file )
 {
   return get_stylesheet_directory_uri() . "/images/$file";
 }
 
+/**
+ * Load a snippet file called $name (assumes file is in the standard theme
+ * snippets directory)
+ * @param  string $name the file name (less ".php")
+ */
 function load_snippet( $name )
 {
   $file = get_stylesheet_directory() . "/snippets/$name.php";
