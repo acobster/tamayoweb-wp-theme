6cdf6ef785bf49bfe315bc9535359ef479512bce
The Ambassador of Funk <acobster@gmail.com>
2014-06-26 12:31:39 -0700

    edit design portfolio link

--- loop-designs.php
+++ loop-designs.php
@@ -1,6 +1,10 @@
 <?php
 
-$designs = get_attachments_by_media_tags( array('media_tags' => 'design') ); ?>
+$designs = get_attachments_by_media_tags( array('media_tags' => 'design') );
+
+if( current_user_can('upload_files') ) : ?>
+  <a class="button tiny" href="/wp/wp-admin/upload.php?media-tags=design">EDIT DESIGN PORTFOLIO</a>
+<?php endif; ?>
 
 <ul class="portfolio design clearing-thumbs" data-clearing>
 
